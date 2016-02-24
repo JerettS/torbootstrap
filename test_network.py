724ff669359ebffbf590cd4a918ac0d479beb639
Sean Butze <sbutze@gmail.com>
2016-02-23 21:36:27 -0500

    update test script

--- test_network.py
+++ test_network.py
@@ -1,9 +1,15 @@
+#
+# Test the Tor network by requesting a web page 
+# through Tor's SOCKS proxy
+#
+
+
 import socket
 import socks
 
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
 s = socks.socksocket()
-s.connect(('google.com', 80))
+s.connect(('tufts.edu', 80))
 
 message = 'GET / HTTP/1.0\r\n\r\n'
 s.sendall(message)
