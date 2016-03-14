b042c44bc073568104a999a620d94ad3df6c4b87
J S <jersupereq@gmail.com>
2016-03-14 23:52:43 +0000

    test.py edited online with Bitbucket

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import subprocess
 from os import system
 
 torcurl = "(time curl -s -o /dev/null --socks5-hostname 127.0.0.1:8666 WEBSITE) 3>&1 1>&2 2>&3 "
-curl = "(time curl -s  -o /dev/null WEBSITE) 3>&1 1>&2 2>&3 "
+curl = "(time curl -s  -o /dev/null WEBSITE ) 3>&1 1>&2 2>&3 "
 reset = "(echo authenticate '\"\"'; echo signal newnym; echo quit) | nc localhost 8667"
 
 
@@ -14,7 +14,7 @@ webfile = open('./tor_bootstrap/websites', 'r')
 def torrunner(website):
 	commandz = torcurl.replace('WEBSITE', website)
 	try:
-		output = "TOR " + website + subprocess.Popen(commandz, shell=True, stdout=subprocess.PIPE).stdout.read()
+		output = "TOR " + website + subprocess.Popen(commandz, shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
 		subprocess.Popen(reset, shell=True, stdout=subprocess.PIPE).stdout.read()
 	except subprocess.CalledProcessError:
 		return "FAILED " + website
@@ -22,50 +22,21 @@ def torrunner(website):
 def regrunner(website):
 	commandz = curl.replace('WEBSITE', website)
 	try:
-		return "REGULAR " + website + subprocess.Popen(commandz, shell=True, stdout=subprocess.PIPE).stdout.read()
+		return "REGULAR " + website + subprocess.Popen(commandz, shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
 	except subprocess.CalledProcessError:
 		return "FAILED " + website
 
-sites = []
 data = webfile.read()
-lines = data.split('\n')
-for line in lines:
-	if line[0] == '#': continue
-	sites.append(line.strip('\n'))
-
-
+sites = data.split('\n')
 
 results = []
 for site in sites:
 	regrunner(site)
 
 for i in range(0,10):
-	results.append(regrunner(site))
-	results.append(torrunner(site))
+	for site in sites:
+ 		results.append(regrunner(site))
+ 		results.append(torrunner(site))
 	
 for result in results:
-	print( result)
-
-# pool_size = 4
-# pool = Pool(processes=pool_size)
-# resultsreg = pool.map(regrunner,sites)
-# print resultsreg
-# resultstor = pool.map(torrunner,sites)
-# print resultstor
-
-# print "RESULTS:"
-# for result in resultsreg:
-# 	print result
-# for result in resultstor:
-# 	print result
- 
-
-
-
-
-
-
-
-
-
-
+	print(result)
